e99cbde11844ad7de91914d94bedecbd0ebdad32
David Bierce <david@bierce.org>
2016-07-18 22:02:00 -0500

    Wrap compose commands to avoid typing compose over and over again.

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,45 @@
 #!/usr/bin/env bash
 
+#!/bin/bash
+set -e
 
-docker-compose build && docker-compose up -d
+case "$1" in
+    build)
+        # Build images based off of the compose file
+        docker-compose build
+        ;;
+    create)
+        # Run compose up based on compose file
+        docker-compose up -d
+        ;;
+    run)
+        # Start containers based on compose file
+        docker-compose start
+        ;;
+    logs)
+        # Follow server logs
+        docker-compose logs --no-color
+        ;;
+    ps)
+        # View running containers based off of compose file
+        docker-compose ps
+        ;;
+    stop)
+        # Stop containers based off of compose file
+        docker-compose stop
+        ;;
+    rm)
+        # Remove containers based off of compose file
+        docker-compose rm
+        ;;
+    kill)
+        # Forcibly stop containers based off of compose file
+        docker-compose kill
+        ;;
+    *)
+        echo "Usage: ./build.sh {build|create|run|logs|ps|rm|kill}"
+        exit 1
+        ;;
+esac
+
+exit 0
